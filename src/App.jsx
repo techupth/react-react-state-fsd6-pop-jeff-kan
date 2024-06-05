80bf59ac8e50248f2c0cefad65cf1c1741cdf8ff
Jeffiros <krittiwat.c@gmail.com>
2024-06-05 11:35:20 +0700

    feat: fill up answers

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [message,setMessage] = useState("Greeting Message")
+  const switchToThai = () => {setMessage("สวัสดี!")};
+  const switchToEnglish = () => {setMessage("Hi!")};
+  const switchToChinese = () => {setMessage("你好!")};
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{message}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={switchToThai}>สวัสดี!</button>
+        <button onClick={switchToEnglish}>Hi!</button>
+        <button onClick={switchToChinese}>你好!</button>
       </div>
     </div>
   );
